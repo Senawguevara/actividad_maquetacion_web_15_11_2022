055bc3ed1081958199c50edb427ec5b4dc56fa49
Formadcol <wguevarac85@gmail.com>
2022-11-17 19:36:03 -0500

    Response

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ function changeCircleColor(color){
    const circle =  document.querySelector('.circle');
    circle.style.background = color;
 }
+
+function toggleMenu(){
+    let menuToggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+    menuToggle.classlist.toggle('active');
+    navigation.classlist.toggle('active');
+}
