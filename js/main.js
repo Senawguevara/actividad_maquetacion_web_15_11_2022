f36b78ab46570559bb3db847b87233e2aa1b3ae4
Formadcol <wguevarac85@gmail.com>
2022-11-18 12:29:44 -0500

    maquetacion_Completa

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
-function imgSlider(anything) {
-    document.querySelector('.starbucks').src = anything;
+addEventListener("DOMContentLoaded", (e)=>{
+  
+    let caja = document.querySelector(".Starbucks");
+    let circulo = document.querySelector(".circle");
+    let img = document.querySelector(".thumb");
+    let menu = document.querySelector(".toggle");
+    let navegacion = document.querySelector(".navigation");
+    let redes = document.querySelector(".sci");
+    let boton = document.querySelector(".content .textBox a");
+    let texto = document.querySelector(".content .textBox h2 span");
     
-}
-
-function changeCircleColor(color){
-   const circle =  document.querySelector('.circle');
-   circle.style.background = color;
-}
-
-function toggleMenu(){
-    let menuToggle = document.querySelector('.toggle');
-    let navigation = document.querySelector('.navigation');
-    menuToggle.classlist.toggle('active');
-    navigation.classlist.toggle('active');
-}
+    img.addEventListener("mouseover", (e)=>{
+        if(e.target.nodeName == "IMG"){
+            caja.src = e.target.dataset.img;
+            let myStyle = {
+                background: e.target.dataset.color
+            };
+            let myStyle2 = {
+                color: e.target.dataset.color
+            };
+            Object.assign(circulo.style, myStyle);
+            Object.assign(redes.style, myStyle);
+            Object.assign(boton.style, myStyle);
+            Object.assign(texto.style, myStyle2);
+        }
+    })
+    menu.addEventListener("click", (e)=>{
+        e.target.classList.toggle("active");
+        navegacion.classList.toggle("active");
+    })
+})
